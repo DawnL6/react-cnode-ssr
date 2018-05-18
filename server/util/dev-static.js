102e3dc6b70c1f2b4a67ede3cc88e434cf4ea89d
17621585885 <>
2018-05-18 18:15:44 +0800

    配置环境和 服务端渲染基础配置

---
+++ server/util/dev-static.js
@@ -0,0 +1,53 @@
+const axios = require('axios')
+const webpack = require('webpack')
+const path = require('path')
+const proxy = require('http-proxy-middleware')
+const MemoryFs = require('memory-fs')
+const ReactDOMServer = require('react-dom/server')
+const serverConfig = require('../../build/webpack.config.server')
+
+const getTempalte = () => {
+    return new Promise((resolve, reject) => {
+        axios.get('http://localhost:8888/public/index.html')
+            .then(res => {
+                resolve(res.data)
+            })
+            .catch(reject)
+    })
+}
+
+const Module = module.constructor
+
+const mfs = new MemoryFs
+const serverCompiler = webpack(serverConfig)
+let serverBundel
+serverCompiler.outputFileSystem = mfs
+serverCompiler.watch({}, (err, stats) => {
+    if (err) throw err
+    stats = stats.toJson()
+    stats.errors.forEach(err => console.error(err))
+    stats.warnings.forEach(warn => console.warn(warn))
+
+    const bundlePath = path.join(
+        serverConfig.output.path,
+        serverConfig.output.filename
+    )
+
+    const bundle = mfs.readFileSync(bundlePath, 'utf-8')
+    const m = new Module()
+    m._compile(bundle, 'server-entry.js')
+    serverBundel = m.exports.default
+})
+module.exports = function (app) {
+    
+    app.use('/public', proxy({
+        target: 'http://localhost:8888'
+    }))
+
+    app.get('*', function (req, res) {
+        getTempalte().then(template => {
+            const content = ReactDOMServer.renderToString(serverBundel)
+            res.send(template.replace('<!-- app -->', content))
+        })
+    })
+}
