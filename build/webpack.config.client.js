bf00949d5f034492c73da92302652907062093bb
17621585885 <>
2018-05-21 10:11:36 +0800

    ellint配置和 忽略 一些不必要文件的上传

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -16,6 +16,12 @@ var config = {
     module: {
         rules: [
             {
+                enforce: 'pre',
+                test: /\.(js|jsx)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            },
+            {
                 test: /\.jsx$/,
                 loader: 'babel-loader',
             },
